4c99c27401a741c1da2ec99d58b26e41f4cfd902
Sjaak Luthart <mail@sjaakluthart.nl>
2017-04-05 18:19:56 +0200

    Removed redundant dependencies.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 /* eslint no-underscore-dangle: ["error", { "allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }] */
 import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
 import io from 'socket.io-client';
 import rootReducer from './reducers/root';
 import createSocketMiddleware from './socketMiddleWare';
@@ -10,7 +9,7 @@ const socket = io('localhost:3000');
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(reduxThunk, createSocketMiddleware(socket))
+  applyMiddleware(createSocketMiddleware(socket))
 );
 
 export default store;
